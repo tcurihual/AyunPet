aba5361ad7d58ff1a3ef71198fe8e83385effef5
sonickiller39 <sonickiller39@gmail.com>
2025-09-09 19:31:21 -0300

    arreglo cosas en mi rama + modificacion del PublicationsContext.tsx

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
-import { LoadingProvider } from './context/LoadingContext';
-import { AuthProvider } from './context/AuthContext';
-import { PublicationsProvider } from './context/PublicationsContext';
+import { LoadingProvider } from './context/LoadingContext.tsx';
+import { AuthProvider } from './context/AuthContext.tsx';
+import { PublicationsProvider } from './context/PublicationsContext'; 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
