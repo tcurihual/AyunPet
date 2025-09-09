9cf20e58343d580bd03ca8fe186471f924ec8191
pvcdf <tu_email@example.com>
2025-09-09 17:33:36 -0300

    contexto login

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,12 +4,15 @@ import App from './App.tsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { LoadingProvider } from './context/LoadingContext';
+import { AuthProvider } from './context/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <LoadingProvider>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </LoadingProvider>
     </BrowserRouter>
   </React.StrictMode>,
