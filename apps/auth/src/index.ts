3764a5f35c5b039d85700e212ecf66f2d7cd7c91
kurovoxx <migueldbzns@gmail.com>
2025-09-12 19:50:13 -0300

    .

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -1,28 +1,30 @@
-import express from "express"
-import cors from "cors"
-import helmet from "helmet"
-import morgan from "morgan"
-import { errorHandler, AUTH_PORT } from "@repo/utils"
-import authRouter from "./routs"
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import { errorHandler, AUTH_PORT } from "@repo/utils";
 
+import userRoutes from "./routes/registerRoute"; 
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(helmet())
-app.use(morgan("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_, res) => {
-    return res.status(200).json({
-        message: "Microservicio Auth funcionando correctamente",
-    })
-})
+  return res.status(200).json({
+    message: "Microservicio Auth funcionando correctamente",
+  });
+});
 
-app.use("/",authRouter)
+// 👇 monta las rutas de usuario en la API
+app.use("/api/auth/users", userRoutes);
 
-app.use(errorHandler)
+app.use(errorHandler);
+
 app.listen(AUTH_PORT, () => {
-    console.log("🚀 Auth service running on http://localhost/api/auth")
-})
+  console.log(`🚀 Auth service running on http://localhost:${AUTH_PORT}/api/auth`);
+});
