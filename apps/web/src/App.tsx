8274f4462449b66ebde8a52388cdbd581c69a4db
sonickiller39 <sonickiller39@gmail.com>
2025-09-22 22:39:56 -0300

    ruta del perfil corregida

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,16 +5,19 @@ import RegisterPage from './pages/RegisterPage';
 import AdoptionRequestsPage from './pages/AdoptionRequestsPage'; 
 import AdoptionPage from './pages/AdoptionPage'; 
 import InstitutionProfilePage from './pages/InstitutionProfilePage';
+import Profile from "./pages/user_profile";
+
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} /> 
-      <Route path="/solicitudes" element={<AdoptionRequestsPage />} />
-      <Route path="/adopta" element={<AdoptionPage />} /> 
-      <Route path="/muro-institucion" element={<InstitutionProfilePage />} />
+      <Route path="/" element={<HomePage/>}/>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/register" element={<RegisterPage/>}/> 
+      <Route path="/solicitudes" element={<AdoptionRequestsPage/>}/>
+      <Route path="/adopta" element={<AdoptionPage/>}/> 
+      <Route path="/muro-institucion" element={<InstitutionProfilePage/>}/>
+      <Route path="/perfil" element={<Profile/>}/>
     </Routes>
   );
 }
