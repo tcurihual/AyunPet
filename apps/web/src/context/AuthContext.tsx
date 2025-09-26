f62a7720c879245bf11690636ae58306f0c91263
sonickiller39 <sonickiller39@gmail.com>
2025-09-26 20:52:48 -0300

    token en el tester y vista todas las solicitudes (debe mejorarse para diferenciar bien)

--- apps/web/src/context/AuthContext.tsx
+++ apps/web/src/context/AuthContext.tsx
@@ -34,6 +34,7 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
       
       let fakeUserData: User | null = null;
       let fakeToken = "";
+      // Corregí los emails para que coincidan con los que hemos usado
       if (data.email === "normal@test.com" && data.password === "password123") {
         fakeUserData = { id: '123', name: 'Usuario de Prueba', email: data.email, role: 'normal' };
         fakeToken = "token_para_usuario_normal_123";
@@ -76,11 +77,15 @@ export const AuthProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
     if (token && role) {
       try {
         await new Promise(resolve => setTimeout(resolve, 1000));
-
-        const userData: User = role === 'institution' 
-          ? { id: '456', name: 'Fundación Sigma', email: 'institucion@test.com', role: 'institution' }
-          : { id: '123', name: 'Usuario de Prueba', email: 'test@test.com', role: 'normal' };
-          
+        let userData: User | null = null;
+        if (role === 'institution') {
+          userData = { id: '456', name: 'Fundación Sigma', email: 'institucion@test.com', role: 'institution' };
+        } else if (role === 'tester') {
+          userData = { id: '789', name: 'Usuario Tester', email: 'tester@test.com', role: 'tester' };
+        } else { 
+          userData = { id: '123', name: 'Usuario de Prueba', email: 'normal@test.com', role: 'normal' };
+        }
+        
         setUser(userData);
       } catch (error) {
         logout();
