0c3fd6bc27e9131992fc40bec7a3f56a6d303136
sonickiller39 <sonickiller39@gmail.com>
2025-09-26 20:02:36 -0300

    bases para grafico barra y torta

---
+++ apps/web/src/components/charts/GraficoBarras.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+interface BarChartData {
+  name: string; 
+  [key: string]: any; 
+}
+
+interface GraficoDeBarrasProps {
+  data: BarChartData[];
+  barKey: string;
+  barColor: string; 
+}
+
+const GraficoDeBarras: React.FC<GraficoDeBarrasProps> = ({ data, barKey, barColor }) => {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart
+        data={data}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" /> 
+        <XAxis dataKey="name" /> 
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey={barKey} fill={barColor} name={barKey.charAt(0).toUpperCase() + barKey.slice(1)} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default GraficoDeBarras;
