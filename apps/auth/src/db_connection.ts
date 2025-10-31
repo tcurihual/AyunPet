5410d847687546bd7d90712fc72466dde9c8f5f4
Mxtsi7 <maxisaezb@gmail.com>
2025-10-31 20:15:03 -0300

    arreglo de rutas

--- apps/auth/src/db_connection.ts
+++ apps/auth/src/db_connection.ts
@@ -1,7 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({
+    path: path.resolve(__dirname, "/.env")
+});
 
 const supabaseUrl = process.env.SUPABASE_URL as string;
 const supabaseKey = process.env.SUPABASE_API_KEY as string;
