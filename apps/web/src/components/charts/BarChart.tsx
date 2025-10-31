54c52ad2615987edbc1306bbe5a6bfa9df85b52f
sonickiller39 <sonickiller39@gmail.com>
2025-10-31 00:49:04 -0300

    base graficos mas ordenada

--- apps/web/src/components/charts/BarChart.tsx
+++ apps/web/src/components/charts/BarChart.tsx
@@ -1,33 +1,25 @@
 import React from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';  
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 interface BarChartData {
-  name: string; 
-  [key: string]: any; 
+  name: string;
+  [key: string]: any;
 }
-
 interface GraficoBarrasProps {
   data: BarChartData[];
   barKey: string;
-  barColor: string; 
+  barColor: string;
 }
-
-const GraficoBarras: React.FC<GraficoBarrasProps> = ({ data, barKey, barColor }) => {
-  return (
-    <ResponsiveContainer width="100%" height={400}>
-      <BarChart
-        data={data}
-        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-      >
-        <CartesianGrid strokeDasharray="3 3" /> 
-        <XAxis dataKey="name" /> 
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey={barKey} fill={barColor} name={barKey.charAt(0).toUpperCase() + barKey.slice(1)} />
-      </BarChart>
-    </ResponsiveContainer>
-  );
-};
-
+const GraficoBarras: React.FC<GraficoBarrasProps> = ({ data, barKey, barColor }) => (
+  <ResponsiveContainer width="100%" height={400}>
+    <BarChart data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Bar dataKey={barKey} fill={barColor} name={barKey.charAt(0).toUpperCase() + barKey.slice(1)} />
+    </BarChart>
+  </ResponsiveContainer>
+);
 export default GraficoBarras;
